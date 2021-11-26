dcc6bd71efd1a17a774002a0ece1484288645c1a
higemania <higemania81@gmail.com>
2021-11-27 00:23:05 +0900

    update javascript scroll

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 'use strinct'
 
-const navbar = document.querySelector("#navbar");
+const navbar = document.querySelector('#navbar');
+// const navbar_menu_item = document.querySelector('.navbar__menu__item');
+const navbarMenu = document.querySelector('.navbar__menu');
 
 document.addEventListener('scroll', ()=> {
     let scrollY         = window.scrollY;
@@ -12,3 +14,17 @@ document.addEventListener('scroll', ()=> {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+navbarMenu.addEventListener('click', (e)=>{
+
+    let section_name    = e.target.textContent.toLowerCase();
+    let obj             = (document.querySelector(`#${section_name}`)) ? document.querySelector(`#${section_name}`) : document.querySelector(`#works`);
+    let section_top     = obj.offsetTop - navbar.getBoundingClientRect().height;
+
+    window.scrollTo( {
+        top: section_top,
+        left: 0,
+        behavior: 'smooth'
+    });
+});
+
