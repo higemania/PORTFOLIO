c389c143634f2581e42e6efb5a3e00806aec2cee
higemania <higemania81@gmail.com>
2021-11-27 16:29:15 +0900

    update javascript arrow-up button

--- main.js
+++ main.js
@@ -5,28 +5,37 @@ const navbar_menu_item  = document.querySelectorAll('.navbar__menu__item'   );
 const navbarMenu        = document.querySelector('.navbar__menu'            );
 const contact           = document.querySelector('.home__contact'           );
 const home_container    = document.querySelector('.home__container'         );
+const arrow_up          = document.querySelector('.arrow__up'               );
 
+let navbarHeight        = navbar.getBoundingClientRect().height;
+let homeHeight          = home_container.getBoundingClientRect().height;
+
+//Navbar Control
 document.addEventListener('scroll', ()=> {
-    let scrollY         = window.scrollY;
-    let navbarHeight    = navbar.getBoundingClientRect().height;
-    let homeHeight      = home_container.getBoundingClientRect().height;
-
-    if(scrollY > navbarHeight){
+    if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     }else{
         navbar.classList.remove('navbar--dark');
     }
-    
+});
+
+//Home Control
+document.addEventListener('scroll', ()=> {
     // let opacity = (homeHeight - scrollY)/homeHeight;
     let opacity = 1 - scrollY/homeHeight;
     if(opacity > 1 || opacity < 0){
         return;
     }
-
     home_container.style.opacity = opacity;
+});
 
-    console.log(`opacity : ${opacity}`);
-    
+//Arrow Btn Control
+document.addEventListener('scroll', ()=> {
+    if(window.scrollY > (homeHeight/2)){
+        arrow_up.classList.add('visible');
+    }else{
+        arrow_up.classList.remove('visible');
+    }
 });
 
 navbarMenu.addEventListener('click', (e) => {
@@ -36,6 +45,10 @@ navbarMenu.addEventListener('click', (e) => {
 contact.addEventListener('click', (e) => {
     scrollTo(e);
 });
+
+arrow_up.addEventListener('click', () => {
+    window.scrollTo( {top: 0, left: 0, behavior: 'smooth'} );
+})
 
 let resetMenuBtn = (target) => {
     navbar_menu_item.forEach((item)=>{
