d31570e4a199ee91e9d10af1ca71861ec8bd4a8e
higemania <higemania81@gmail.com>
2021-11-27 14:53:03 +0900

    update javascript contace me

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 'use strinct'
 
-const navbar = document.querySelector('#navbar');
-// const navbar_menu_item = document.querySelector('.navbar__menu__item');
-const navbarMenu = document.querySelector('.navbar__menu');
+const navbar            = document.querySelector('#navbar');
+const navbar_menu_item  = document.querySelectorAll('.navbar__menu__item');
+const navbarMenu        = document.querySelector('.navbar__menu');
+const contact           = document.querySelector('.home__contact');
 
 document.addEventListener('scroll', ()=> {
     let scrollY         = window.scrollY;
@@ -15,16 +16,32 @@ document.addEventListener('scroll', ()=> {
     }
 });
 
-navbarMenu.addEventListener('click', (e)=>{
-
-    let section_name    = e.target.textContent.toLowerCase();
-    let obj             = (document.querySelector(`#${section_name}`)) ? document.querySelector(`#${section_name}`) : document.querySelector(`#works`);
-    let section_top     = obj.offsetTop - navbar.getBoundingClientRect().height;
-
-    window.scrollTo( {
-        top: section_top,
-        left: 0,
-        behavior: 'smooth'
-    });
+navbarMenu.addEventListener('click', (e) => {
+    scrollTo(e);
 });
+
+contact.addEventListener('click', (e) => {
+    scrollTo(e);
+});
+
+let resetMenuBtn = (target) => {
+    navbar_menu_item.forEach((item)=>{
+        item.classList.remove('active');
+    });
+
+    target.classList.add('active');
+}
+
+let scrollTo = (e) => {
+    if(!e.target.dataset.link) {
+        return;
+    }
+
+    resetMenuBtn(e.target);
+
+    let target_section = document.querySelector(e.target.dataset.link);
+    let section_top    = target_section.offsetTop - navbar.getBoundingClientRect().height;
+
+    window.scrollTo( {top: section_top, left: 0, behavior: 'smooth'} );
+}
 
